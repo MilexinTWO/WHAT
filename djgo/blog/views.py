ff42067165866bdde29006fb40c872523809363c
unknown <youhcu101010@gmail.com@example.com>
2023-08-30 17:14:17 +0800

    ain

---
+++ djgo/blog/views.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import render
+import requests
+
+
+def index(request):
+    response = requests.get(
+        "https://emma.pixnet.cc/mainpage/blog/categories/hot/28") #28為國內旅遊
+    article = response.json()["articles"]#轉換為JSON物件後，存取articles欄位
+    # print( article )
+    # #print( article.user.display_name)
+    # print( article.title )
+    # print( article.link)
+    # print( article.link)
+
+    return render(request, "index.html", {"articles": article})
---
+++ py.py
@@ -0,0 +1,12 @@
+
+import requests
+
+response = requests.get(
+    "https://emma.pixnet.cc/mainpage/blog/categories/hot/28") #28為國內旅遊
+article = response.json()["articles"]#轉換為JSON物件後，存取articles欄位
+print( response.json() )
+# #print( article.user.display_name)
+# print( article.title )
+# print( article.link)
+# print( article.link)
+
